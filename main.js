56b6a56e623e4a6fce00182af9e2fcf4cd5d1ba2
David Everett <davideverett1989@gmail.com>
2020-10-05 14:28:14 -0500

    wip

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 // Section 2: Timing Our Code
 
+// const addUpTo = (n) => {
+//     let total = 0;
+//     for (let i = 1; i <= n; i++) {
+//         total += i;
+//     }
+//     return total;
+// }
+
+// const t1 = performance.now();
+// console.log(addUpTo(100000000));
+// const t2 = performance.now();
+
+
 const addUpTo = (n) => {
-    let total = 0;
-    for (let i = 1; i <= n; i++) {
-        total += i;
-    }
-    return total;
+    return n * (n + 1) / 2;
 }
 
 const t1 = performance.now();
-console.log(addUpTo(100000000));
+console.log(addUpTo(200));
 const t2 = performance.now();
 
 console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds`);
