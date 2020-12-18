353656f4bc60bf5e8abed8b51ec79eb88298161b
David Everett <davideverett1989@gmail.com>
2020-12-17 22:40:03 -0600

    wip

--- main.js
+++ main.js
@@ -90,3 +90,55 @@ const isAlphaNumeric = (char) => {
 };
 
 charCounter('Hello world!#$@#!@@@');
+
+// Problem Solving Patterns:
+
+// 1. Frequency Counter:
+
+// Use objects and avoid nested loops.
+
+// const same = (arr1, arr2) => {
+//     if (arr1.length !== arr2.length) {
+//         return false;
+//     }
+//     return arr1.every((x, i)=> {
+//         const square = Math.pow(x, 2);
+//         if (arr2.includes(square)) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     });
+// };
+
+console.clear();
+
+// function same(arr1, arr2) {
+//     if (arr1.length !== arr2.length) return false;
+//     for (let i = 0; i < arr1.length; i++) {
+//         const correctIndex = arr2.indexOf(Math.pow(arr1[i], 2));
+//         if (correctIndex === -1) return false;
+//         arr2.splice(correctIndex, 1);
+//     }
+//     return true;
+// };
+
+function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+    for (let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+    }
+    for (let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+    }
+    for (let key in frequencyCounter1) {
+        if (!(Math.pow(key, 2) in frequencyCounter2)) return false;
+        if (frequencyCounter2[Math.pow(key, 2)] !== frequencyCounter1[key]) return false;
+    }
+    return true;
+};
+
+
+console.log(same([2, 2, 4], [4, 16]));
