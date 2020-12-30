83fb8293508c528bb74cb85da1f956f44d431e46
David Everett <davideverett1989@gmail.com>
2020-12-29 23:50:34 -0600

    wip

--- main.js
+++ main.js
@@ -376,3 +376,66 @@ function findLongestSubstring(str) {
   }
 
 console.log(findLongestSubstring('rithmschool'));
+
+// Recursion
+
+console.clear();
+
+function countDown(num) {
+    if (num <= 0) {
+        console.log('All Done.');
+        return;
+    }
+    console.log(num);
+    num--;
+    countDown(num);
+}
+
+countDown(50);
+
+console.clear();
+
+function sumRange(num) {
+    console.log(num);
+    if (num === 1) return 1;
+    return num + sumRange(num - 1);
+}
+
+console.log('Result:', sumRange(10));
+
+console.clear();
+
+// Iterative Factorial Solution
+
+// function factorial(num) {
+//     let total = 1;
+//     for (let i = num; i > 1; i--) {
+//         total *= i;
+//     }
+//     return total;
+// }
+
+// Recursive Factorial Solution
+
+function factorial(num) {
+    if (num === 1) return 1;
+    return num * factorial(num -1);
+}
+
+console.log('Result:', factorial(5));
+
+console.clear();
+
+function collectOddValues(arr) {
+    let result = [];
+
+    function helper(input) {
+        if (input.length === 0) return;
+        if (input[0] % 2 !== 0) result.push(input[0]);
+        helper(input.slice(1));
+    }
+    helper(arr);
+    return result;
+}
+
+console.log('Result:' ,collectOddValues([2, 3, 4, 5, 6, 7, 8, 9]));
