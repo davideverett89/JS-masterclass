8eca5277230f701383a8396dd5a3a9d842cd371f
David Everett <davideverett1989@gmail.com>
2020-12-30 00:11:17 -0600

    Chapter 7 Done.

--- main.js
+++ main.js
@@ -426,16 +426,29 @@ console.log('Result:', factorial(5));
 
 console.clear();
 
-function collectOddValues(arr) {
-    let result = [];
+// Helper Method Recusion
 
-    function helper(input) {
-        if (input.length === 0) return;
-        if (input[0] % 2 !== 0) result.push(input[0]);
-        helper(input.slice(1));
-    }
-    helper(arr);
-    return result;
+// function collectOddValues(arr) {
+//     let result = [];
+
+//     function helper(input) {
+//         if (input.length === 0) return;
+//         if (input[0] % 2 !== 0) result.push(input[0]);
+//         helper(input.slice(1));
+//     }
+//     helper(arr);
+//     return result;
+// }
+
+// Pure Recursion
+
+function collectOddValues(arr) {
+    let newArr = [];
+    if (arr.length === 0) return newArr;
+    if (arr[0] % 2 !== 0) newArr.push(arr[0]);
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
 }
 
-console.log('Result:' ,collectOddValues([2, 3, 4, 5, 6, 7, 8, 9]));
+console.log('Result:' ,collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
